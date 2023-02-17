6ea7a0552cbdf67eb1ce74808f7b60239f917804
A510_1 <saha.mars2012@gmail.com>
2023-02-17 20:31:28 +0300

    HIVM

---
+++ Invest/Controllers/AccountController.cs
@@ -0,0 +1,114 @@
+﻿using Invest.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Invest.Service;
+using System.Web;
+using MimeKit;
+using MailKit.Net.Smtp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Invest.Controllers
+{
+
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly SignInManager<IdentityUser> signInManager;
+        public AccountController(UserManager<IdentityUser> userMgr, SignInManager<IdentityUser> signinMgr)
+        {
+            userManager = userMgr;
+            signInManager = signinMgr;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.UserName, Email = model.Email };
+                var result = await userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+
+                    EmailService emailService = new EmailService();
+                    await emailService.SendEmailAsync(user.Email, "Подтверждение регистрации на сайте компании СтройИнвест23", "Здравствуйте, " + user.UserName + "." + " Благодарим за регистрацию на сайте ! " + "Для завершения регистрации перейдите по ссылке: " + "<br>" + "https://localhost:7133/Account/ConfirmEmail?id=" + user.Id);
+                    // установка куки
+                    System.Threading.Thread.Sleep(1674);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        public async Task<ActionResult> ConfirmEmail(string id)
+        {
+            IdentityUser user = await userManager.FindByIdAsync(id);
+            user.EmailConfirmed = true;
+            await userManager.UpdateAsync(user);
+            await signInManager.SignInAsync(user, isPersistent: false);
+            return RedirectToAction("Index", "Home");
+        }
+        [Authorize]
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View(new LoginViewModel());
+        }
+        [Authorize]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser user = await userManager.FindByNameAsync(model.UserName);
+                if (user != null)
+                {
+                    if (user.EmailConfirmed == true)
+                    {
+                        await signInManager.SignOutAsync();
+                        Microsoft.AspNetCore.Identity.SignInResult result = await signInManager.PasswordSignInAsync(user, model.Password, model.RememberMe, false);
+                        if (result.Succeeded)
+                        {
+                            return Redirect(model.returnUrl ?? "/");
+                        }
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginViewModel.UserName), "Неверные данные");
+            }
+            return View(model);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        [Authorize]
+        public async Task<IActionResult> Profile()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Invest/Service/EmailServicecs.cs
@@ -0,0 +1,30 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using System.Threading.Tasks;
+namespace Invest.Service
+{
+    public class EmailService
+    {
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            using var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("СтройИнвест23", "afisheshelper@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = message
+            };
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync("smtp.mail.ru", 465, true);
+                await client.AuthenticateAsync("afisheshelper@mail.ru", "ebwyWvEw6gcCs4YzV7yB");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
