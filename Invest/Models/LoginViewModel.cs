13a006298097ec134a17193f0a4d448b8bd1dca7
A510_1 <saha.mars2012@gmail.com>
2023-02-17 20:17:05 +0300

    1

---
+++ Invest/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Invest.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string UserName { get; set; }
+
+        [Required]
+        [UIHint("password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+        public string? returnUrl { get; set; }
+    }
+}
