39a27f7cd5df3122c97faf9b33038fd52ea01094
A510_1 <saha.mars2012@gmail.com>
2023-02-18 16:54:45 +0300

    Python

---
+++ Telebot-main/bot.py
@@ -0,0 +1,166 @@
+import telebot
+import sql
+
+from telebot import types
+from multiprocessing import Process
+from time import sleep
+
+bot_token = '6131879353:AAGKP8nmK-6kksTxJWtymxCBgIWCIihOchs'
+
+bot = telebot.TeleBot(bot_token)
+
+info = []
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    my_id_button = types.KeyboardButton('Мой id')
+    favorites_button = types.KeyboardButton('Избранное')
+    help_button = types.KeyboardButton('Техподдержка')
+    markup.add(my_id_button, favorites_button, help_button)
+    bot.send_message(message.from_user.id, "👋 Привет! Я твой бот-помощник!", reply_markup=markup)
+
+
+def gen_markup():
+    markup = types.InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(types.InlineKeyboardButton("🠔", callback_data="cb_left"),
+               types.InlineKeyboardButton("🠖", callback_data="cb_right"))
+    return markup
+
+
+def create_caption(co):
+    address = ""
+
+    if co['address'] is not None:
+        address += co['address']
+
+    if co['stage'] is not None:
+        address += f", этаж {co['stage']}"
+
+    if co['number'] is not None:
+        address += f", квартира {co['number']}"
+
+    if co['are_ready']:
+        ready = "Введён в эксплуатацию"
+    else:
+        ready = "Не введён в эксплуатацию"
+
+    if co['is_repaired']:  # ремонт
+        repair = "Есть"
+    else:
+        repair = "Нет"
+
+    caption = f"Адрес: {address}\n" \
+              f"Площадь: {co['area']}\n" \
+              f"Применение: {co['usage']}\n" \
+              f"Тип: {co['type']}\n" \
+              f"Готовность: {ready}\n" \
+              f"Ремонт: {repair}\n" \
+              f"Цена: {co['price']}\n" \
+              f"Перспективность: {co['perspective']}\n" \
+              f"Риск: {co['risk']}\n"
+
+    return caption
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    chat_id = message.chat.id
+
+    text = message.text
+
+    if text == 'Мой id':
+        bot.send_message(chat_id, f'Ваш id: {chat_id}')
+
+    elif text == 'Избранное':
+
+        user_name = sql.check_usr(chat_id)
+
+        if user_name is not None:
+
+            global info
+            info = sql.favorite(user_name, False)
+
+            if len(info) != 0:
+                caption = create_caption(info[0])
+                bot.send_photo(chat_id=chat_id,
+                               caption=f'1 из {len(info)}\n{caption}',
+                               photo=info[0]['image'],
+                               reply_markup=gen_markup())
+
+        else:
+            bot.send_message(chat_id, "Пожалуйста, авторизуйтесь на сайте через ваш telegram-id")
+
+    elif text == 'Техподдержка':
+        bot.send_message(chat_id, 'Ожидайте оператора')
+        # Вызов оператора технической поддержки
+
+    else:
+        bot.send_message(chat_id, 'Извините, запрос не распознан')
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    i = int(call.message.caption[:call.message.caption.find(' ')])
+
+    if call.data == "cb_left":
+
+        if i != 1:
+
+            i -= 1
+            caption = create_caption(info[i - 1])
+
+            bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                   media=types.InputMediaPhoto(info[i - 1]['image']), reply_markup=gen_markup())
+
+            bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                     caption=f'{i} из {len(info)}\n{caption}', reply_markup=gen_markup())
+        else:
+            return
+
+    elif call.data == "cb_right":
+
+        if i != len(info):
+
+            i += 1
+            caption = create_caption(info[i - 1])
+
+            bot.edit_message_media(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                   media=types.InputMediaPhoto(info[i - 1]['image']), reply_markup=gen_markup())
+
+            bot.edit_message_caption(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                     caption=f'{i} из {len(info)}\n{caption}', reply_markup=gen_markup())
+
+        else:
+            return
+
+
+def bot_work():
+    bot.polling(none_stop=True, interval=0)
+
+
+def update():
+    while True:
+        sleep(30)
+        lib = sql.check()
+        for key, value in lib.items():
+            if len(value) != 0:
+                bot.send_message(key, "Обратите внимание на обновление вашего избранного")
+                for co in value:
+                    caption = create_caption(co)
+                    bot.send_photo(chat_id=key,
+                                   caption=caption,
+                                   photo=co['image'])
+
+
+if __name__ == '__main__':
+    pr_bot = Process(target=bot_work)
+    pr_update = Process(target=update)
+
+    pr_bot.start()
+    pr_update.start()
+
+    pr_bot.join()
+    pr_update.join()
---
+++ Telebot-main/sql.py
@@ -0,0 +1,78 @@
+import pyodbc
+import PIL.Image as Image
+import io
+
+server = 'SQL8004.site4now.net'
+database = 'db_a93836_invest'
+username = 'db_a93836_invest_admin'
+password = 'elmelm327327'
+cnxn = pyodbc.connect(
+    'DRIVER={ODBC Driver 18 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=yes;UID=' + username + ';PWD=' + password)
+
+
+def img(path):
+    io_bytes = io.BytesIO(path)
+    i = Image.open(io_bytes)
+    return i
+
+
+def check_usr(user_id):
+    user_id = str(user_id)
+    cursor = cnxn.cursor()
+    cursor.execute('select UserName, PhoneNumber from dbo.AspNetUsers')
+    for row in cursor.fetchall():
+        if row[1] == user_id:
+            return row[0]
+    return None
+
+
+def favorite(user_name, push):
+    cursor = cnxn.cursor()
+    cursor.execute('select StateId, UserName, IsChanged from dbo.Favorites')
+    states_id = []
+    for row in cursor.fetchall():
+        if (user_name == row[1]) and (not push or row[2]):
+            states_id.append(row[0])
+    cursor = cnxn.cursor()
+    cursor.execute('select * from dbo.RealEstates')
+    info = dict(dict())
+    for row in cursor.fetchall():
+        if row[0] in states_id:
+            l = {
+                'address': row[1],
+                'area': row[5],
+                'stage': row[3],
+                'perspective': row[12],
+                'risk': '',
+                'usage': row[6],
+                'are_ready': row[8],
+                'number': row[2],
+                'type': row[7],
+                'price': float(row[4]) * float(row[5]),
+                'is_repaired': row[14]
+            }
+            info[row[0]] = l
+
+    cursor.execute('select _Image, StateId from dbo.Images')
+    a = []
+    for row in cursor.fetchall():
+        if row[1] not in a:
+            a.append(row[1])
+            if row[1] in info:
+                info[row[1]]['image'] = img(row[0])
+
+    return list(info.values())
+
+
+def check():
+    cursor = cnxn.cursor()
+    cursor.execute('select UserName, PhoneNumber from dbo.AspNetUsers')
+    users = dict()
+    for row in cursor.fetchall():
+        users[row[0]] = row[1]
+    lib = dict()
+    for k, v in users.items():
+        lib[v] = favorite(k, True)
+    cursor.execute(f'update dbo.Favorites set IsChanged = 0')
+    cnxn.commit()
+    return lib
