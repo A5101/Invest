b5c53b71af89ea61f0811529ef98358a38595c98
Кирилл Слисенко <slisenko02@mail.ru>
2023-02-19 06:55:24 +0300

    Indicators + SearchMap

--- Invest/Controllers/RealEstateController.cs
+++ Invest/Controllers/RealEstateController.cs
@@ -28,55 +28,16 @@ namespace Invest.Controllers
 
         public IActionResult Show(Guid id)
         {
-            double R = 0;
-            
-            double T = 0;
             var astate = dataManager.RealAstates.GetRealEstateItemById(id);
-            astate.Risks = 0;
-            astate.Liquid = 0.5;
 
             string addr = astate.Address;
-            if (astate.Floor != "") 
-            { 
+            if (astate.Floor != null)
+            {
                 addr += "Этаж " + astate.Floor;
-                int i = int.Parse(astate.Floor);
-                if (i == 1) R -= 0.1;
-                else if (i < 4) R -= 0.05;
             }
-            R += 0.5 * astate.NearPlaces;
-            if (astate.Usage == "Коммерческое") R += 0.25;
-            if (astate.NearWorsth)R -= 5;
-            T = astate.Cost * astate.Area * R;
-            if (!astate.AreReady)
-            {
-                float proc = (float)(100 * T / astate.CostCadastr);
-                if (astate.Usage == "Жилое")
-                {
-                    if (proc < 0.06) astate.Risks += (float)0.5;
-                    else if (proc < 0.08) astate.Risks += 1;
-                    else if (proc < 0.12) astate.Risks += 2;
-                    else astate.Risks += 3;
-                } else
-                {
-                    if (proc < 0.16) astate.Risks += (float)0.5;
-                    else if (proc < 0.2) astate.Risks += 1;
-                    else if (proc < 0.4) astate.Risks += 2;
-                    else astate.Risks += 3;
-                }
-            }
-            astate.Liquid += astate.NearPlaces * 0.02;
-            if (int.Parse(astate.Floor) > 16) astate.Liquid -= 0.3;
-            else if (int.Parse(astate.Floor) > 5) astate.Liquid += 0.05;
-            else astate.Liquid -= 0.15;
-            if (astate.NearWorsth) astate.Liquid -= 0.1;
 
-            if (astate.Isrepaired) astate.Liquid += 0.1; else astate.Liquid -= 0.1;
+            if (astate.Number != "") addr +="Квартира " + astate.Number;
 
-            if (astate.AreReady)
-            {
-                if (astate.Liquid < 0.2) astate.Risks += (float)1.5;
-                else if (astate.Liquid < 0.4) astate.Risks += 1;
-            }
 
             List<string> exp = astate.Expenses.Split(',').ToList();
             double total = 0;
@@ -85,16 +46,6 @@ namespace Invest.Controllers
             {
                 total += int.Parse(exp[i].Substring(exp[i].IndexOf(':') + 1, exp[i].Length - exp[i].IndexOf(':') - 1));
             }
-
-            if (astate.Usage == "Жилое")
-                total += astate.CostCadastr * 0.003 / astate.Liquid;
-            else
-                total += astate.CostCadastr * 0.02 / astate.Liquid;
-
-
-            astate.Raiting = astate.Cost * astate.Area * 5 / astate.Liquid / (total + astate.Cost * astate.Area);
-
-            if (astate.Number != "") addr +="Квартира " + astate.Number;
 
             var plan = dataManager.Images.GetPlanImageByAstateId(id);
             var im = dataManager.Images.GetPlanImagesByAstateId(id);
@@ -123,16 +74,82 @@ namespace Invest.Controllers
         }
 
         [HttpPost]
-        public IActionResult Edit(RealEstate model, string name )
+        public IActionResult Edit(RealEstate astate, string name )
         {
             if (ModelState.IsValid)
             {
-                model.OwnerName = name;
-                dataManager.RealAstates.SaveRealAstatesItem(model);
-                dataManager.HistoryPrices.AddHistory(model.Id, (int)model.Area * model.Cost, DateTime.UtcNow);
+                astate.OwnerName = name;
+                double R = 0;
+
+                double T = 0;
+
+                astate.Risks = 0;
+                astate.Liquid = 0.5;
+
+                if (astate.Floor != null)
+                {
+                    int i = int.Parse(astate.Floor);
+                    if (i == 1) R -= 0.1;
+                    else if (i < 4) R -= 0.05;
+                }
+                R += 0.5 * astate.NearPlaces;
+                if (astate.Usage == "Коммерческое") R += 0.25;
+                if (astate.NearWorsth) R -= 5;
+                T = astate.Cost * astate.Area * R;
+                if (!astate.AreReady)
+                {
+                    float proc = (float)(100 * T / astate.CostCadastr);
+                    if (astate.Usage == "Жилое")
+                    {
+                        if (proc < 0.06) astate.Risks += (float)0.5;
+                        else if (proc < 0.08) astate.Risks += 1;
+                        else if (proc < 0.12) astate.Risks += 2;
+                        else astate.Risks += 3;
+                    }
+                    else
+                    {
+                        if (proc < 0.16) astate.Risks += (float)0.5;
+                        else if (proc < 0.2) astate.Risks += 1;
+                        else if (proc < 0.4) astate.Risks += 2;
+                        else astate.Risks += 3;
+                    }
+                }
+                astate.Liquid += astate.NearPlaces * 0.02;
+                if (int.Parse(astate.Floor) > 16) astate.Liquid -= 0.3;
+                else if (int.Parse(astate.Floor) > 5) astate.Liquid += 0.05;
+                else astate.Liquid -= 0.15;
+                if (astate.NearWorsth) astate.Liquid -= 0.1;
+
+                if (astate.Isrepaired) astate.Liquid += 0.1; else astate.Liquid -= 0.1;
+
+                if (astate.AreReady)
+                {
+                    if (astate.Liquid < 0.2) astate.Risks += (float)1.5;
+                    else if (astate.Liquid < 0.4) astate.Risks += 1;
+                }
+
+                List<string> exp = astate.Expenses.Split(',').ToList();
+                double total = 0;
+
+                for (int i = 0; i < exp.Count; i++)
+                {
+                    total += int.Parse(exp[i].Substring(exp[i].IndexOf(':') + 1, exp[i].Length - exp[i].IndexOf(':') - 1));
+                }
+
+                if (astate.Usage == "Жилое")
+                    total += astate.CostCadastr * 0.003 / astate.Liquid;
+                else
+                    total += astate.CostCadastr * 0.02 / astate.Liquid;
+
+
+                astate.Raiting = astate.Cost * astate.Area * 5 / astate.Liquid / (total + astate.Cost * astate.Area);
+
+
+                dataManager.RealAstates.SaveRealAstatesItem(astate);
+                dataManager.HistoryPrices.AddHistory(astate.Id, (int)astate.Area * astate.Cost, DateTime.UtcNow);
                 return base.RedirectToAction(nameof(Invest.Controllers.HomeController.Index), nameof(Invest.Controllers.HomeController).CutController());
             }
-            return View(model);
+            return View(astate);
         }
 
         public IActionResult AddImages(Guid id, IFormFileCollection coll)
